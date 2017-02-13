77e16ebe8bfa9a084be59557c05256be77bb1095
George Codat Tudoran <office@solcreation.ro>
2017-02-13 11:14:11 +0200

    Set start location and calculate distance from home

--- Assets/LogInfo.cs
+++ Assets/LogInfo.cs
@@ -5,9 +5,14 @@ public class LogInfo : MonoBehaviour {
 
 	// Use this for initialization
 	void Start () {
-        print("Fire up the game!");
-        print("Set up the environment");
-	}
+        float location = 3.0f; // Distance in meters
+        float homeLocation = 2.4f;
+        float distance = homeLocation - location;
+        print("Welcome to Go Home!");
+        print("A game were you need to find your way back home");
+        print("Distance: ");
+        print(distance);
+    }
 	
 	// Update is called once per frame
 	void Update () {
