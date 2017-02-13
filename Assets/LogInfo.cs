48c8560f27d16d960693f5c018ccebaa4f76ba0c
George Codat Tudoran <office@solcreation.ro>
2017-02-13 11:26:33 +0200

    Update player position

--- Assets/LogInfo.cs
+++ Assets/LogInfo.cs
@@ -3,17 +3,30 @@ using System.Collections;
 
 public class LogInfo : MonoBehaviour {
 
-	// Use this for initialization
-	void Start () {
-        float location = 3.0f; // Distance in meters
+    // Use this for initialization
+    void Start() {
+        float location = 2.4f; // Distance in meters
         float homeLocation = 2.4f;
         float distance = homeLocation - location;
         print("Welcome to Go Home!");
         print("A game were you need to find your way back home");
         print("Distance: ");
         print(distance);
+        if (location > homeLocation)
+        {
+            print("Player needs to go backwards ");
+        }
+
+        if (location < homeLocation)
+        {
+            print("Player needs to go forward ");
+        }
+
+        if (location == homeLocation)
+        {
+            print("Player is at home ");
+        }
     }
-	
 	// Update is called once per frame
 	void Update () {
 	
