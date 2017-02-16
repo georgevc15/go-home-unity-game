d5b6c888e69684f06318aa92c92b27c6fcc8b049
George Codat Tudoran <office@solcreation.ro>
2017-02-16 11:38:39 +0200

    Update player position on key press

--- Assets/LogInfo.cs
+++ Assets/LogInfo.cs
@@ -1,34 +1,42 @@
 ﻿using UnityEngine;
 using System.Collections;
 
-public class LogInfo : MonoBehaviour {
+public class LogInfo : MonoBehaviour
+{
+
+    public Vector2 location; // Distance in meters (x,y)
+    public Vector2 homeLocation;
 
     // Use this for initialization
-    void Start() {
-        float location = 2.4f; // Distance in meters
-        float homeLocation = 2.4f;
-        float distance = homeLocation - location;
+    void Start()
+    {
         print("Welcome to Go Home!");
         print("A game were you need to find your way back home");
-        print("Distance: ");
-        print(distance);
-        if (location > homeLocation)
-        {
-            print("Player needs to go backwards ");
-        }
-
-        if (location < homeLocation)
-        {
-            print("Player needs to go forward ");
-        }
-
-        if (location == homeLocation)
-        {
-            print("Player is at home ");
-        }
     }
-	// Update is called once per frame
-	void Update () {
-	
-	}
-}
+
+    // Update is called once per frame
+    void Update()
+    {
+        CheckForMovement(KeyCode.LeftArrow, new Vector2(-1, 0));
+        CheckForMovement(KeyCode.RightArrow, new Vector2(1, 0));
+        CheckForMovement(KeyCode.UpArrow, new Vector2(0, 1));
+        CheckForMovement(KeyCode.DownArrow, new Vector2(0, -1));
+    }
+
+    void CheckForMovement(KeyCode kc, Vector2 movementVector) {
+        if (Input.GetKeyDown(kc))
+            {
+                location = location + movementVector;
+                Vector2 pathToHome = homeLocation - location;
+                print("Distance to home " + pathToHome.magnitude);
+                if (location == homeLocation)
+                {
+                    print("You have arrived home! ");
+                }
+
+              }
+          }
+
+
+   }
+
